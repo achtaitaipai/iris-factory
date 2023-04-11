b1cae2187d87f23da2e877a77b9eca552e4a6e51
achtaitaipai <charles.cailleteau@gmail.com>
2023-04-11 22:48:31 +0200

    pwa

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './app.css'
 import App from './App.svelte'
 
+// const worker = new Worker(new URL('./worker/index.js', import.meta.url))
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/iris-factory/sw.js')
+}
 const app = new App({
 	target: document.getElementById('app'),
 })
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	build: {
+		rollupOptions: {
+			output: {
+				// Prevent vendor.js being created
+				entryFileNames: '[name].js',
+				// this got rid of the hash on style.css
+				assetFileNames: '[name].[ext]',
+			},
+		},
+		outDir: 'iris-factory',
+	},
 	base: '/iris-factory',
 })
