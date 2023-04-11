0ad7c98abe8b8ccdc7acea2db7c92dc4b06e3e27
achtaitaipai <charles.cailleteau@gmail.com>
2023-04-11 22:15:33 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+const urlsToCache = [
+	'/iris-factory/',
+	'/iris-factory/index.js',
+	'/iris-factory/dragDropTouch.js',
+	'/iris-factory/index.css',
+]
+
+const cacheName = 'iris-factory-0'
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(cacheName).then((cache) => {
+			return cache.addAll(urlsToCache)
+		})
+	)
+})
+
+self.addEventListener('activate', (e) => {
+	e.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key === cacheName) {
+						return
+					}
+					return caches.delete(key)
+				})
+			)
+		})
+	)
+})
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		fetch(event.request).catch((error) => {
+			return caches.match(event.request)
+		})
+	)
+})
